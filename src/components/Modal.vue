ffbdfe94ce39f365969e77e425a644ff909103a2
rlawlals119 <rlawlals170@naver.com>
2025-04-08 17:42:56 +0900

    feat,fix: pinia, modal 컴포넌트 추가, header 수정

---
+++ src/components/Modal.vue
@@ -0,0 +1,20 @@
+<!-- Modal.vue -->
+<template>
+  <div class="modal-overlay" v-if="visible">
+    <div class="modal">
+      <button class="close-button" @click="hideModal">X</button>
+      <div class="modal-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useModalStore } from '@/stores/modal';
+const modalStore = useModalStore();
+const { showModal, hideModal } = modalStore;
+
+const visible = computed(() => modalStore.isModalVisible);
+</script>
