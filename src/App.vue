a35568bac41b0a2d35400e0effa69eff40b06b3c
rlawlals119 <rlawlals170@naver.com>
2025-04-08 13:43:38 +0900

    feat SignupPage UI 작성

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div><HomePage></HomePage></div>
+  <div>
+    <!-- <HomePage></HomePage> -->
+    <RouterLink to="/signup">회원가입</RouterLink>
+  </div>
+  <RouterView />
 </template>
 
 <script setup>
+import { RouterLink, RouterView } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
+import SignupPage from './pages/SignupPage.vue';
 </script>
 
 <style lang="scss" scoped></style>
