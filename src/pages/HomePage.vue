82b8b2d043b722a626615d91ea779e17870b034b
NanaManBo <kimbosung22@naver.com>
2025-04-08 18:04:18 +0900

    feat : 홈페이지 영상 추가가

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,28 @@
-
 <template>
-  <div class="p-8 bg-yellow-100 text-2xl font-bold text-center">
-    Tailwind 적용됨 🎉
-  </div>
+  <!-- 배경 비디오 -->
+  <figure
+    id="id"
+    class="relative w-full h-screen overflow-hidden m-0"
+  >
+    <video
+      id="myVideo"
+      src="@/assets/background.mp4"
+      autoplay
+      muted
+      loop
+      class="w-full h-full object-cover"
+    ></video>
+
+    <!-- 비디오 위 텍스트 -->
+    <article
+      class="text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center"
+    >
+      <p class="text-4xl font-bold leading-tight">
+        MOA <br />
+      </p>
+      <p>모두가 자산을 모을 때까지 MOA와 함께하세요</p>
+    </article>
+  </figure>
 </template>
 
 <script setup></script>
